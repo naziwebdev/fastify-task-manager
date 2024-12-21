cd778a479a0fe01fd12c2b964d72c2003429ee01
nazi <nazipaeez777@gmail.com>
2024-12-21 23:27:30 +0330

    Config cors

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const fastifyCors = require("fastify-cors");
 const { db } = require("./db");
 const configs = require("./configs");
 
+fastify.register(fastifyCors, {
+  origin: "*",
+  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  preflightContinue: true,
+  optionsSuccessStatus: 204,
+});
+
 const start = async () => {
   try {
     await db.authenticate();
