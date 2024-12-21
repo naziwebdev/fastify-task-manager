3bd58c71889a0bf415dfa02799bf1a97eee676af
nazi <nazipaeez777@gmail.com>
2024-12-21 23:08:40 +0330

    Init sequelize

---
+++ configs.js
@@ -0,0 +1,18 @@
+module.exports = {
+    db: {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        name: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        uri: process.env.DB_URI,
+        poolSize: process.env.DB_POOL_SIZE,
+      },
+    
+      port: parseInt(process.env.PORT) || 4006,
+    
+      auth: {
+        accessTokenSecretKey: process.env.ACCESS_TOKEN_SECRET_KEY,
+      },
+}
---
+++ db.js
@@ -0,0 +1,17 @@
+const {Sequelize} = require('sequelize')
+const configs = require('./configs')
+
+
+const db = new Sequelize({
+    host: configs.db.host,
+    port: configs.db.port,
+    username: configs.db.user,
+    password: configs.db.password,
+    database: configs.db.name,
+    dialect: configs.db.dialect,
+    logging: console.log,
+})
+
+
+
+module.exports = {db}
