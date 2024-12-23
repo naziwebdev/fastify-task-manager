43a24bc759d1f90ab93e25a62affa524730b9e9e
nazi <nazipaeez777@gmail.com>
2024-12-23 20:25:53 +0330

    Develope auth services funcs

--- services/user.js
+++ services/user.js
@@ -1,23 +1,37 @@
+const { User } = require("../db");
+const { Op } = require("sequelize");
 
 
-const createUser = async (username,phone,email,password) => {
-try {
-    
-} catch (error) {
-    throw new Error(error)
-}
-}
 
-
+const createUser = async (username, phone, email, password) => {
+  try {
+    const user = await User.create(
+      { username, phone, email, password },
+      { raw: true }
+    );
+    return user;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 const getUserById = async (id) => {
-    try {
-    
-    } catch (error) {
-        throw new Error(error)
-    }
-}
+  try {
+    const user = await User.findOne({ where: { id }, raw: true });
+    return user;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
+const getUserByPhoneOrEmail = async (phone, email) => {
+  const user = await User.findOne({
+    where: {
+      [Op.or]: [{ email }, { phone }],
+    },
+  });
 
+  return user;
+};
 
-module.exports = {createUser}
+module.exports = { createUser, getUserById , getUserByPhoneOrEmail};
